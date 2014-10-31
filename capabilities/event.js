d5dee3fe1a6beaec67c942a4d601d691673e657f
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2014-10-31 14:48:18 +0100

    Event Capabilities

--- capabilities/event.js
+++ capabilities/event.js
@@ -8,30 +8,36 @@ module.exports = function(object) {
 
     var events = {};
 
-    object.on = function(identifier, fnc) {
-        events[identifier] = events[identifier] || [];
-        events[identifier].push(fnc);
+    Object.defineProperty(object, "on", {
+        value: function(identifier, fnc) {
+            events[identifier] = events[identifier] || [];
+            events[identifier].push(fnc);
 
-        return object;
-    };
-
-    object.off = function(identifier, fnc) {
-        if (identifier in events === true) {
-            events[identifier].splice(events[identifier].indexOf(fnc), 1);
+            return object;
         }
+    });
 
-        return object;
-    };
-
-    object.emit = function(identifier, fnc) {
-        if (identifier in events === true) {
-            for (var i = 0; i < events[identifier].length; i++) {
-                events[identifier][i].apply(object, slice.call(arguments, 1));
+    Object.defineProperty(object, "removeListener", {
+        value: function(identifier, fnc) {
+            if (identifier in events === true) {
+                events[identifier].splice(events[identifier].indexOf(fnc), 1);
             }
-        }
 
-        return object;
-    };
+            return object;
+        }
+    });
+
+    Object.defineProperty(object, "emit", {
+        value: function(identifier, fnc) {
+            if (identifier in events === true) {
+                for (var i = 0; i < events[identifier].length; i++) {
+                    events[identifier][i].apply(object, slice.call(arguments, 1));
+                }
+            }
+
+            return object;
+        }
+    });
 
     return object;
 
