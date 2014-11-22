323bf373df07872a0d265bfe01d571eca410042c
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2014-11-22 23:29:09 +0100

    Handle control points

--- draw/point.js
+++ draw/point.js
@@ -3,13 +3,23 @@ require('../core/model.js')(Point);
 function Point (x, y, controlPoints) {
     this.x = x;
     this.y = y;
-    this.controlPoints = controlPoints || null;
+
+    this.addControlPoints(controlPoints || []);
+    this.controlPoints = [];
 }
 
 
 (function (proto) {
 
-    PointProto.def('addControlPoint', function () {
+    proto.def('addControlPoints', function (controlPoints) {
+
+        for (var i = 0; i < controlPoints.length; i++) {
+            if (i > 1) {
+                break;
+            }
+
+            this.controlPoints[i] = Point.create(controlPoints[i]);
+        }
 
     });
 
