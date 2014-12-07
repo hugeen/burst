4e99c1cc999389d5c5cc37f6650b27c846a0b317
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2014-12-07 13:48:25 +0100

    Polyfills

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+require('./dom/polyfills/animation')(window);
+
 var $ = require('./dom/query');
 var Canvas = require('./draw/canvas');
 var Point = require('./geometry/point');
@@ -7,12 +9,15 @@ var path = new Path([
     new Point(10, 10),
     new Point(10, 100),
     new Point(100, 100),
-    new Point(100, 10)
+    new Point(100, 10),
+    new Point(10, 10)
 ]);
 
 
 $(function () {
     var canvas = new Canvas($('canvas')[0]);
     canvas.draw(path);
-    console.log(path);
+    requestAnimationFrame(function() {
+        console.log('hello');
+    });
 });
