2626b8e045c939e94a0c4c36e5db6db47c96b712
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2014-12-11 21:22:40 +0100

    Bad way to handle curves

--- draw/canvas/draw_path.js
+++ draw/canvas/draw_path.js
@@ -11,63 +11,13 @@ function drawPath (path) {
     this.context.beginPath();
 
     for (var i = 0; i < path.points.length; i++) {
-        point = path.points[i];
-
-        var operation = selectDrawingOperation(point, i);
-        var drawingArgs = getDrawingArgs.call(point, operation);
-
-        this.context[operation].apply(this.context, drawingArgs);
+        var point = path.points[i];
+        var operation = !i ? 'moveTo' : 'lineTo';
+        this.context[operation].apply(this.context, [point.x, point.y]);
     }
 
     this.context.stroke();
 
     return this;
 
-}
-
-
-function selectDrawingOperation (point, index) {
-
-    var operations = ['lineTo', 'quadraticCurveTo', 'bezierCurveTo'];
-
-    return !index ? 'moveTo' : operations[point.controlPoints.length];
-}
-
-
-function getDrawingArgs (operation) {
-
-    var point = this;
-
-    var operations = {
-        moveTo: function () {
-            return [point.x, point.y];
-        },
-        bezierCurveTo: function () {
-            var cp1 = point.controlPoints[0];
-            var cp2 = point.controlPoints[1];
-
-            return [
-                point.x + cp1.x,
-                point.y + cp1.y,
-                point.x + cp2.x,
-                point.y + cp2.y,
-                point.x,
-                point.y
-            ];
-        },
-        quadraticCurveTo: function () {
-            var cp = point.controlPoints[0];
-
-            return [
-                point.x + cp.x,
-                point.y + cp.y,
-                point.x,
-                point.y
-            ];
-        }
-    };
-
-    operations.lineTo = operations.moveTo;
-
-    return operations[operation]();
 }
--- geometry/point.js
+++ geometry/point.js
@@ -1,51 +1,12 @@
 require('../tween/tween_point')(Point);
 
 
-function Point (x, y, controlPoints) {
+function Point (x, y) {
+    this.type = 'Point';
+
     this.x = x;
     this.y = y;
-
-    this.controlPoints = controlPoints || [];
 }
-
-
-Point.prototype.toArray = function () {
-    var controlPoints = [];
-
-    for (var i = 0; i < this.controlPoints.length; i++) {
-        controlPoints.push(this.controlPoints[i].toArray());
-    }
-
-    return [this.x, this.y, controlPoints];
-};
-
-
-Point.prototype.fromArray = function (raw) {
-    this.x = raw[0];
-    this.y = raw[1];
-    console.log(raw);
-    for (var i = 0; i < this.controlPoints.length; i++) {
-        this.controlPoints[i].fromArray(raw[2][i]);
-    }
-    console.log(this);
-};
-
-
-Point.prototype.clone = function () {
-    var controlPoints = [];
-
-    for (var i = 0; i < this.controlPoints.length; i++) {
-        controlPoints.push(this.controlPoints[i].clone());
-    }
-
-    return new Point(this.x, this.y, controlPoints);
-};
-
-
-Point.prototype.translate = function (x, y) {
-    this.x += x;
-    this.y += y;
-};
 
 
 module.exports = Point;
