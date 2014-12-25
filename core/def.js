3a44d0d90c0c62f45270d1caa16a45dce0cf6cb2
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2014-12-25 21:21:44 +0100

    Cooler def

--- core/def.js
+++ core/def.js
@@ -1,7 +1,7 @@
 module.exports = function(object) {
 
     if (!('def' in object)) {
-        def.call(this, 'def', {value: def});
+        def.call(this, 'def', def);
     }
 
     return object;
@@ -9,5 +9,9 @@ module.exports = function(object) {
 
 
 function def (name, settings) {
+    if (typeof settings === 'function') {
+        settings = {value: settings};
+    }
+
     return Object.defineProperty(this, name, settings);
 }
