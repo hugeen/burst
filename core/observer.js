1a1cc6cd56782a9abc64230d5ee062bf7e9fae11
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2014-12-26 11:12:32 +0100

    Observer

---
+++ core/observer.js
@@ -0,0 +1,48 @@
+var slice = Array.prototype.slice;
+var eventCapabilities = require('./event');
+
+module.exports = function (object) {
+
+    if ('observe' in object) {
+        return object;
+    }
+
+    defineObservedProperties(object);
+
+    object.subscribe = subscribe;
+    object.unsubscribe = unsubscribe;
+
+    return object;
+
+};
+
+
+function defineObservedProperties (object) {
+    Object.defineProperty(object, 'observedProperties', {
+        value: {}
+    });
+}
+
+
+function subscribe (property) {
+    if (!(property in this.observedProperties) || !this.observedProperties[property]) {
+        this.observedProperties[property] = true;
+
+        Object.defineProperty(this, property, {
+            set: function (newValue) {
+                this.emit(property + ' changed', newValue);
+                return newValue;
+            }
+        });
+    }
+}
+
+function unsubscribe (property) {
+    this.observedProperties[property] = false;
+
+    Object.defineProperty(this, property, {
+        set: function (newValue) {
+            return newValue;
+        }
+    });
+}
