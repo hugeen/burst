d94485691465e93e3f89dac5ff93e223169d5697
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2014-12-29 20:50:48 +0100

    GamepadAbstract (refactor needed)

--- gamepad_abstract/detection.js
+++ gamepad_abstract/detection.js
@@ -1,15 +1,23 @@
 var sharedProperties = ['axes', 'buttons', 'id', 'mapping', 'timestamp'];
 
+// Refactor that shit
+module.exports = function(GamepadAbstract) {
 
-module.exports = function (GamepadAbstract) {
-
-    window.addEventListener('gamepadconnected', function (e) {
+    window.addEventListener('gamepadconnected', function(e) {
         var gamepad = new GamepadAbstract();
         proxifyProperty(e.gamepad, gamepad, sharedProperties);
+        for (var i = 0; i < gamepad.buttons.length; i++) {
+            var button = gamepad.buttons[i];
+            notifierCapabilities(button);
+            button.subscribe('pressed');
+            button.on('pressed changed', function(value) {
+                gamepad.emit('button ' + (value ? 'pressed' : 'released'), button);
+            });
+        }
     });
 
 
-    window.addEventListener('gamepaddisconnected', function (e) {
+    window.addEventListener('gamepaddisconnected', function(e) {
         var gamepad = GamepadAbstract.find(e.gamepad.id);
         GamepadAbstract.remove(gamepad);
     });
@@ -19,17 +27,17 @@ module.exports = function (GamepadAbstract) {
 };
 
 
-function proxifyProperties (source, destination, properties) {
-    for (var i = 0; i < properties.length; i ++) {
+function proxifyProperties(source, destination, properties) {
+    for (var i = 0; i < properties.length; i++) {
         proxifyProperty(source, destination, properties[i]);
     }
 }
 
 
-function proxifyProperty (source, destination, property) {
+function proxifyProperty(source, destination, property) {
     Object.defineProperty(destination, property, {
         enumerable: true,
-        get: function () {
+        get: function() {
             return source[property];
         }
     });
