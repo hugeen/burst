6eb25214e376efada4d5ab1fc0297af66aad0d8c
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-02-23 23:29:14 +0100

    More robust DomQuery

--- glowing_dom/dom_query.js
+++ glowing_dom/dom_query.js
@@ -1,20 +1,15 @@
 export default class DomQuery extends Array {
 
 	constructor (selector) {
-		super();
-		this.push(document.querySelectorAll(selector));
+		this.push.apply(this, document.querySelectorAll(selector));
 	}
 
 	on (name, fnc) {
-		this.forEach.call(this[0], function (el) {
-			el.addEventListener(name, fnc);
-		});
+		this.forEach(el => el.addEventListener(name, fnc));
 	}
 
 	removeListener (name, fnc) {
-		this.forEach.call(this[0], function (el) {
-			el.removeEventListener(name, fnc);
-		});
+		this.forEach(el => el.removeEventListener(name, fnc));
 	}
 
 }
