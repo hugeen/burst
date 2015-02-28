647bd2217e8c647af3de46814cc340bc5d92e343
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-02-28 01:36:13 +0100

    HttpRequest

---
+++ glowing_http/http_request.js
@@ -0,0 +1,37 @@
+import eventAbilities from '../glowing_core/event_abilities';
+import {addEventProxy} from '../glowing_core/event_utils';
+
+export default class HttpRequest {
+
+	constructor (settings) {
+
+		eventAbilities(this);
+		Object.assign(this, settings);
+
+    	this.xhr = new XMLHttpRequest();
+    	this.proxifyEvents([
+			['progress'],
+			['load', 'loaded'],
+			['error', 'failure'],
+			['aborted']
+		]);
+
+    	this.open();
+
+	}
+
+	open () {
+		this.xhr.open(this.method, this.url, true);
+	}
+
+	send () {
+		this.xhr.send();
+	}
+
+	proxifyEvents (identifiers) {
+		for (var identifier of identifiers) {
+			addEventProxy([this.xhr, 'addEventListener'], this, ...identifier);
+		}
+	}
+
+}
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import eventAbilities from 'glowing_core/event_abilities';
 import dirtyAbilities from 'glowing_core/dirty_abilities';
 import {addEventProxy, removeEventProxy} from 'glowing_core/event_utils';
+import HttpRequest from 'glowing_http/http_request';
+
 var object1 = {};
 var object2 = {};
 
@@ -11,9 +13,15 @@ object1.on('hello', function () {
 	console.log('hello');
 });
 
-var proxy = addEventProxy([object2, 'on'], [object1, 'emit'], 'hello bitch', 'hello');
-removeEventProxy([object2, 'removeListener'], 'hello bitch', proxy);
+var proxy = addEventProxy(object2, object1, 'hello bitch', 'hello');
+removeEventProxy(object2, 'hello bitch', proxy);
 
 object2.emit('hello bitch');
 
+var http = new HttpRequest({ method: 'get' });
+http.on('loaded', function () {
+	console.log('h');
+});
+
+http.send();
 export default {};
