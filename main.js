e6a115cf10cc4c4a1c55fd69511b440c037bf681
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-02-28 14:39:27 +0100

    Update main

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import animLoop from 'glowing_animation/animation_loop';
 import $ from 'glowing_dom/dom_query';
+import keyboard from 'glowing_dom/keyboard';
+
+keyboard.on('key down', function (e) {
+	console.log(e);
+});
+
 
 var body = $('body');
 console.log(body);
