ac830e0abc32c8314f2b80300ae7df05f1e58cb7
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-04-07 23:24:17 +0200

    Element Attributes

--- specs/dom/manipulation_spec.js
+++ specs/dom/manipulation_spec.js
@@ -130,4 +130,36 @@ specs.push(function () {
     return assert(isStyled, 'should set many style properties on an element');
 });
 
+
+specs.push(function () {
+    reset();
+    var element = document.createElement('h1');
+    element.setAttribute('data-hello', 'world');
+
+    return assert(dom.getAttribute(element, 'data-hello') === 'world', 'should get an attribute on an element');
+});
+
+
+specs.push(function () {
+    reset();
+    var element = document.createElement('h1');
+    dom.setAttribute(element, 'data-hello', 'world');
+
+    return assert(element.getAttribute('data-hello') === 'world', 'should set an attribute on an element');
+});
+
+
+specs.push(function () {
+    reset();
+    var element = document.createElement('h1');
+    dom.setAttributes(element, {
+        'data-hello': 'world',
+        'data-foo': 'bar'
+    });
+    var isSet = element.getAttribute('data-hello') === 'world' && element.getAttribute('data-foo') === 'bar';
+
+    return assert(isSet, 'should set many attributes on an element');
+});
+
+
 export default {name: 'DOM manipulation', specs};
