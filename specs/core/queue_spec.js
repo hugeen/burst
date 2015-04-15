9bd830e41f2ff8819788df56de9170722e292b97
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-04-15 22:09:52 +0200

    Minimalize queue

--- specs/core/queue_spec.js
+++ specs/core/queue_spec.js
@@ -1,5 +1,4 @@
 import assert from 'core/assert';
-import {on} from 'core/event';
 import {processQueue} from 'core/queue';
 
 
@@ -21,18 +20,6 @@ specs.push(function (done) {
     var queue = [increment, increment, increment];
     processQueue(queue);
     done(assert(queue, 'should start queue'));
-});
-
-
-specs.push(function (done) {
-    reset();
-    var queue = [increment, increment, increment];
-    var completed = false;
-    on(queue, 'complete', function () {
-        completed = true;
-    });
-    processQueue(queue);
-    done(assert(completed, 'should emit an event on complete'));
 });
 
 
