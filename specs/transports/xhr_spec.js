3fbc504ab4c7518d1e61e055b2a358ac5a8cbf21
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-04-16 22:45:49 +0200

    Better XHR

--- specs/transports/xhr_spec.js
+++ specs/transports/xhr_spec.js
@@ -1,6 +1,6 @@
 import assert from 'core/assert';
 import {on} from 'core/event';
-import {xhrEvents, buildXhr, proxifyXhrEvent, proxifyXhrEvents} from 'transports/xhr';
+import {xhrEventsMap, buildXhr, proxifyXhrEvent, proxifyXhrEvents} from 'transports/xhr';
 
 
 var specs = [];
@@ -33,21 +33,6 @@ specs.push(function (done) {
     xhr.dispatchEvent(e);
 
     done(assert(passed, 'should proxify XHR event'));
-});
-
-
-specs.push(function (done) {
-    reset();
-    var xhr = new XMLHttpRequest();
-
-    proxifyXhrEvents(xhr, xhrEvents);
-    xhrEvents.forEach(function (eventName) {
-        var e = new Event(eventName);
-        on(xhr, eventName, increment);
-        xhr.dispatchEvent(e);
-    });
-
-    done(assert(passed === xhrEvents.length, 'should proxify XHR events'));
 });
 
 
