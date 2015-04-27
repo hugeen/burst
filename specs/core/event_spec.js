5eeb43cbce779ab823ee2ac8756b50973081d5f4
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-04-27 12:41:45 +0200

    Refactor event specs

--- specs/core/event_spec.js
+++ specs/core/event_spec.js
@@ -1,151 +1,131 @@
-import assert from 'core/assert';
+import {describe, it, beforeEach, should} from 'test/spec';
+
 import {
     on, emit, removeListener, getListeners,
     globalOn, globalEmit, globalRemoveListener, globalListeners
 } from 'core/event';
 
 
-var specs = [];
+describe('Event', function () {
+
+    var mock;
+    var passed;
 
 
-var mock;
-var passed;
-
-function reset () {
-    mock = {};
-    passed = 0;
-}
+    function increment () {
+        passed += 1;
+    }
 
 
-function increment () {
-    passed += 1;
-}
-
-
-specs.push(function (done) {
-    reset();
-
-    on(mock, 'event name', increment);
-    emit(mock, 'event name');
-
-    done(assert(passed, 'should register and trigger listener'));
-});
-
-
-specs.push(function (done) {
-    reset();
-
-    on(mock, 'event name', increment);
-    var listeners = getListeners(mock, 'event name');
-
-    done(assert(listeners.length, 'should get listeners'));
-});
-
-
-specs.push(function (done) {
-    reset();
-
-    on(mock, 'event name', increment);
-    on(mock, 'event name', increment);
-    on(mock, 'event name', increment);
-    emit(mock, 'event name');
-
-    done(assert(passed === 3, 'should trigger multiple listeners'));
-});
-
-
-specs.push(function (done) {
-    reset();
-
-    on(mock, 'event name', increment);
-    removeListener(mock, 'event name', increment);
-    emit(mock, 'event name');
-
-    done(assert(!passed, 'should remove a listener'));
-});
-
-
-specs.push(function (done) {
-    reset();
-
-    on(mock, 'event name', increment);
-    removeListener(mock, 'event name', function() {});
-    var listeners = getListeners(mock, 'event name');
-
-    done(assert(listeners.length, 'should not remove listener'));
-});
-
-
-specs.push(function (done) {
-    reset();
-
-    var param1;
-    var param2;
-
-    on(mock, 'custom', function (p1, p2) {
-        param1 = p1;
-        param2 = p2;
+    beforeEach(function () {
+        mock = {};
+        passed = 0;
     });
 
-    emit(mock, 'custom', true, false);
 
-    done(assert(param1 && !param2, 'should forward parameters'));
-});
+    it('should register and trigger listener', function () {
+        on(mock, 'event name', increment);
+        emit(mock, 'event name');
 
-
-specs.push(function (done) {
-    reset();
-
-    var array = [1, 2, 3];
-    var count;
-
-    on(array, 'custom', function () {
-        this.push(4);
+        should(passed);
     });
 
-    on(array, 'custom', function () {
-        this.push(5);
+
+    it('should get listeners', function () {
+        on(mock, 'event name', increment);
+        var listeners = getListeners(mock, 'event name');
+
+        should(listeners.length);
     });
 
-    emit(array, 'custom');
 
-    done(assert(array.length === 5, 'should work with array'));
-});
+    it('should trigger multiple listeners', function () {
+        on(mock, 'event name', increment);
+        on(mock, 'event name', increment);
+        on(mock, 'event name', increment);
+        emit(mock, 'event name');
 
-
-specs.push(function (done) {
-    reset();
-
-    on(increment, 'custom', function () {
-        this();
+        should(passed === 3);
     });
 
-    emit(increment, 'custom');
 
-    done(assert(passed, 'should work with functions'));
+    it('should remove a listener', function () {
+        on(mock, 'event name', increment);
+        removeListener(mock, 'event name', increment);
+        emit(mock, 'event name');
+
+        should(!passed);
+    });
+
+
+    it('should not remove listener', function () {
+        on(mock, 'event name', increment);
+        removeListener(mock, 'event name', function() {});
+        var listeners = getListeners(mock, 'event name');
+
+        should(listeners.length);
+    });
+
+
+    it('should forward parameters', function () {
+        var param1;
+        var param2;
+
+        on(mock, 'custom', function (p1, p2) {
+            param1 = p1;
+            param2 = p2;
+        });
+
+        emit(mock, 'custom', true, false);
+
+        should(param1 && !param2);
+    });
+
+
+    it('should work with array', function () {
+        var array = [1, 2, 3];
+        var count;
+
+        on(array, 'custom', function () {
+            this.push(4);
+        });
+
+        on(array, 'custom', function () {
+            this.push(5);
+        });
+
+        emit(array, 'custom');
+
+        should(array.length === 5);
+    });
+
+
+    it('should work with functions', function () {
+        on(increment, 'custom', function () {
+            this();
+        });
+
+        emit(increment, 'custom');
+
+        should(passed);
+    });
+
+
+    it('should register and trigger global listener', function () {
+        globalOn('event name', increment);
+        globalEmit('event name');
+        globalRemoveListener('event name', increment);
+
+        should(passed);
+    });
+
+
+    it('should trigger global listener on local listener added', function () {
+        globalOn('listener added', increment);
+        on(mock, 'event name', function () {});
+        globalRemoveListener('listener added', increment);
+
+        should(passed);
+    });
+
 });
-
-
-specs.push(function (done) {
-    reset();
-
-    globalOn('event name', increment);
-    globalEmit('event name');
-    globalRemoveListener('event name', increment);
-
-    done(assert(passed, 'should register and trigger global listener'));
-});
-
-
-specs.push(function (done) {
-    reset();
-
-    globalOn('listener added', increment);
-    on(mock, 'event name', function () {});
-    globalRemoveListener('listener added', increment);
-
-    done(assert(passed, 'should trigger global listener on local listener added'));
-});
-
-
-export default {name: 'Event', specs};
