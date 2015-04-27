c96794077d04346b15b8a2479a96194be02da588
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-04-27 12:44:57 +0200

    Better specs runner

--- specs/specs_runner.js
+++ specs/specs_runner.js
@@ -1,86 +1,73 @@
-import assertSpecs from 'specs/core/assert_spec';
-import eventSpecs from 'specs/core/event_spec';
-import queueSpecs from 'specs/core/queue_spec';
-import animationLoopSpecs from 'specs/time/animation_loop_spec';
-import domTraversingSpecs from 'specs/dom/traversing_spec';
-import domEventsSpecs from 'specs/dom/events_spec';
-import domManipulationSpecs from 'specs/dom/manipulation_spec';
-import domUtilsSpecs from 'specs/dom/utils_spec';
-import domDimensionsSpecs from 'specs/dom/dimensions_spec';
-import domAccessorsSpecs from 'specs/dom/accessors_spec';
-import httpSpecs from 'specs/transport/http_spec';
-import mediaSpecs from 'specs/transport/media_spec';
-import keyboardSpecs from 'specs/input/keyboard_spec';
+// import 'specs/core/queue_spec';
+// import 'specs/time/animation_loop_spec';
+// import 'specs/dom/traversing_spec';
+// import 'specs/dom/events_spec';
+// import 'specs/dom/manipulation_spec';
+// import 'specs/dom/utils_spec';
+// import 'specs/dom/dimensions_spec';
+// import 'specs/dom/accessors_spec';
+// import 'specs/transport/http_spec';
+// import 'specs/transport/media_spec';
+// import 'specs/input/keyboard_spec';
 
 
-runNextSet([
-    assertSpecs,
-    eventSpecs,
-    queueSpecs,
-    animationLoopSpecs,
-    domTraversingSpecs,
-    domEventsSpecs,
-    domManipulationSpecs,
-    domUtilsSpecs,
-    domDimensionsSpecs,
-    domAccessorsSpecs,
-    httpSpecs,
-    mediaSpecs,
-    keyboardSpecs
-]);
+import 'specs/core/event_spec';
+import {run} from 'test/spec';
+
+run();
 
 
-var specsCount = 0;
-var totalPassed = 0;
+// var specsCount = 0;
+// var totalPassed = 0;
 
-function runNextSet(sets, i = 0) {
-    if (i < sets.length) {
-        var set = sets[i];
-        console.group(`%c ${set.name} `, 'font-size: 1.3em;');
+// function runNextSet(sets, i = 0) {
+//     if (i < sets.length) {
+//         var set = sets[i];
+//         console.group(`%c ${set.name} `, 'font-size: 1.3em;');
 
-        runNextSpec(set, 0, function () {
-            logResult({count: set.specs.length, passed: set.passed});
-            console.groupEnd();
-            runNextSet(sets, i + 1);
-        });
-    }
-}
-
-
-function runNextSpec(set, i, callback) {
-
-    if (i >= set.specs.length) {
-        callback();
-    } else {
-        set.specs[i](function(result) {
-            output(result);
-            if (result.passed) {
-                set.passed = (set.passed || 0) + 1;
-            }
-            runNextSpec(set, i + 1, callback);
-        });
-    }
-}
+//         runNextSpec(set, 0, function () {
+//             logResult({count: set.specs.length, passed: set.passed});
+//             console.groupEnd();
+//             runNextSet(sets, i + 1);
+//         });
+//     }
+// }
 
 
-function output(result) {
-    if (result.passed) {
-        console.log(result.infos.message);
-    } else {
-        console.error(result.infos.stack);
-    }
-}
+// function runNextSpec(set, i, callback) {
+
+//     if (i >= set.specs.length) {
+//         callback();
+//     } else {
+//         set.specs[i](function(result) {
+//             output(result);
+//             if (result.passed) {
+//                 set.passed = (set.passed || 0) + 1;
+//             }
+//             runNextSpec(set, i + 1, callback);
+//         });
+//     }
+// }
 
 
-function logResult(result, label = '') {
-    var {
-        count, passed
-    } = result;
-    var allPassed = count === passed;
-    var style = 'font-weight: bold;';
-    if (!allPassed) {
-        style += ' color: red;';
-    }
+// function output(result) {
+//     if (result.passed) {
+//         console.log(result.infos.message);
+//     } else {
+//         console.error(result.infos.stack);
+//     }
+// }
 
-    console[allPassed ? 'info' : 'warn'](`%c ${label}${passed}/${count}`, style);
-}
+
+// function logResult(result, label = '') {
+//     var {
+//         count, passed
+//     } = result;
+//     var allPassed = count === passed;
+//     var style = 'font-weight: bold;';
+//     if (!allPassed) {
+//         style += ' color: red;';
+//     }
+
+//     console[allPassed ? 'info' : 'warn'](`%c ${label}${passed}/${count}`, style);
+// }
