d8aa41b2200b4c569150be2adf2e4356bd89696a
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-06-27 18:36:09 +0200

    With notification center

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var exec = require('child_process').exec;
+var notifier = require('node-notifier');
+
+
+gulp.task('default', function () {
+    gulp.watch('./test/*/*.js', ['testem']);
+});
 
 
 gulp.task('build:tests', function() {
@@ -12,9 +18,12 @@ gulp.task('build:tests', function() {
 
 
 gulp.task('testem', ['build:tests'], function (cb) {
-  exec('./node_modules/testem/testem.js -l Firefox ci', function (err, stdout, stderr) {
+  exec('./node_modules/testem/testem.js -g -l Firefox ci', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
+    if(err) {
+        notifier.notify({title: 'Burst test suite failed', message: stdout, sound: 'Sosumi'});
+    }
     cb(err);
   });
 });
