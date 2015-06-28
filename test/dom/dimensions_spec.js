d601b208a43b569eda147b7f1e31b872113d11b7
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-06-28 13:33:04 +0200

    DOM dimensions specs

---
+++ test/dom/dimensions_spec.js
@@ -0,0 +1,71 @@
+import assert from 'assert';
+import * as dom from '../../dom/dimensions';
+
+
+describe('DOM Dimensions', function () {
+
+
+    var container;
+
+    beforeEach(function () {
+        document.body.innerHTML = '';
+        container = document.createElement('div');
+        document.body.appendChild(container);
+        container.style.height = '';
+        container.style.width = '';
+        container.style.innerHTML = '';
+        container.style.position = 'absolute';
+        container.style.top = '40px';
+        container.style.left = '50px';
+    });
+
+
+    it('should get element height', function () {
+        container.style.height = '40px';
+        assert(dom.getHeight(container) === 40);
+    });
+
+
+    it('should get element width', function () {
+        container.style.width = '50px';
+        assert(dom.getWidth(container) === 50);
+    });
+
+
+    it('should get element size', function () {
+        container.style.width = '50px';
+        container.style.height = '40px';
+
+        var {width, height} = dom.getSize(container);
+        assert(height === 40 && width === 50);
+    });
+
+
+    it('should get element position', function () {
+        var div = document.createElement('div');
+        container.appendChild(div);
+
+        var {top, left} = dom.getPosition(div);
+        assert(top === 0 && left === 0);
+    });
+
+
+    it('should get element offset', function () {
+        var div = document.createElement('div');
+        container.appendChild(div);
+
+        var {top, left} = dom.getOffset(div);
+        assert(top === 40 && left === 50);
+    });
+
+
+    it('should get element position from viewport', function () {
+        var div = document.createElement('div');
+        container.appendChild(div);
+
+        var {top, left} = dom.getPositionFromViewport(container);
+        assert(top === 40 && left === 50);
+    });
+
+
+});
--- test/suite.js
+++ test/suite.js
@@ -1,4 +1,5 @@
 import './core/event_spec';
 import './dom/manipulation_spec';
 import './dom/traversing_spec';
+import './dom/dimensions_spec';
 import './time/animation_loop_spec';
