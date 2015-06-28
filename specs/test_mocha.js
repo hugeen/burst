26ebcedc11607a1cd2dafc90f2c986af5360d82b
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-06-28 13:24:26 +0200

    Remove useless file

--- specs/test_mocha.js
+++
@@ -1,9 +0,0 @@
-
-import assert from "assert"
-import {on} from '../lib/core/event' // importing a module
-
-describe('something', () => { // using the arrow function
-  it('that should work', () => {
-    assert.equal(-1, [1,2,3].indexOf(5));
-  });
-});
