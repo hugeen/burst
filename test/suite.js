20e40a483f070c918bcf97e2521631835565cfbd
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-06-28 15:26:41 +0200

    Add dom specs for travis

--- test/suite.js
+++ test/suite.js
@@ -1,10 +1,10 @@
 import './core/event_spec';
-// import './dom/manipulation_spec';
-// import './dom/traversing_spec';
-// import './dom/dimensions_spec';
-// import './dom/accessors_spec';
-// import './dom/event_spec';
-// import './dom/utils_spec';
+import './dom/manipulation_spec';
+import './dom/traversing_spec';
+import './dom/dimensions_spec';
+import './dom/accessors_spec';
+import './dom/event_spec';
+import './dom/utils_spec';
 // import './time/animation_loop_spec';
 // import './input/keyboard_spec';
 // import './transport/http_spec';
