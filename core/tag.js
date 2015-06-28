0b1449d2ee0ff548eee3b4d3b4cd63ce9a0d3d69
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-06-28 13:19:47 +0200

    Remove some core useless files

--- core/tag.js
+++
@@ -1,5 +0,0 @@
-hasTag(object, 'pending');
-isTagged(object);
-addTag(object, 'pending'),
-removeTag(object, 'pending');
-toggleTag(object, 'pending');
--- core/utils.js
+++
@@ -1,6 +0,0 @@
-export function getParamNames (fnc) {
-    var string = fnc.toString();
-    var names = string.match(/\(.*?\)/)[0].replace(/[()]/gi,'').replace(/\s/gi,'');
-
-    return names === '' ? [] : names.split(',');
-};
