50affe4350b342d5846248f70ed76948b3cea708
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2015-10-04 23:35:53 +0200

    Exp

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function () {
+    var options = {
+      "animate": true,
+      "patternWidth": 250,
+      "patternHeight": 250,
+      "grainOpacity": 0.15,
+      "grainDensity": 1,
+      "grainWidth": 1,
+      "grainHeight": 1
+    }
+    grained('#hero', options)
+})
